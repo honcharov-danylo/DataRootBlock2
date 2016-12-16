9e8feb8dd06416917d20e9718f35a0164f4196c0
mr. noone <smth@smth.com>
2016-12-16 19:49:11 +0200

    Small changes to code from learningpython. My svm code not working yet.

--- svm/svm.py
+++ svm/svm.py
@@ -1,5 +1,3 @@
-#doesn't work for now, need's to be repaired. Or rewrited complitely
-
 import gradient.gradient as gradient
 import numpy as np
 import regression.regression as regression
@@ -7,63 +5,64 @@ from bokeh.plotting import figure, output_file, save,show
 
 import math
 
-# class Function_for_svm(gradient.Function_class):
-#     def __init__(self,model):
-#         self.model=model
-#
-#     def function(self, param):
-#         first_part=sum(param)
-#         #second_part=0.5*math.pow(sum([a*x*y for y in self.model.keys() for x in self.model[y] for a in param]),2)
-#         second_part=0
-#         for l in param:
-#             for y in self.model.keys():
-#                 for x_vect in self.model[y]:
-#                     for x in x_vect:
-#                         second_part+=l*y*x
-#         return first_part-second_part*second_part
-#
-#     def derivative(self, param):
-#         return sum([a*y for y in self.model.keys() for a in param])
-#
-#     def derivative_by_part_x(self, x, index_of_x, vector_of_x):
-#         sum=0
-#         for l in vector_of_x:
-#             for y in self.model.keys():
-#                 for i in range(len(self.model[y])):
-#                     if i != index_of_x:
-#                         for x in self.model[y][i]:
-#                             sum += l * y * x
-#
-#                         #return sum([a*current_x*y for y in self.model.keys() for current_x in [x_vect for xvect in filter(self.model[y],lambda l: l!=x)] for a in param])
-#         return sum
-#
-#     def get_w(self,lambdas):
-#         w=[]
-#         for l in lambdas:
-#             for y in self.model.keys():
-#                 for x_vect in self.model[y]:
-#                     for x in x_vect:
-#                         w.append(x*y*l)
-#         return w
-#     def get_b(self, lambdas):
-#         #b=1/list(model.keys())[0]-sum([x*w for x in self.model[list(self.model.keys())[0]] for w in koefs])
-#         koefs=self.get_w(lambdas)
-#         b=1/list(self.model.keys())[0]
-#         for w in koefs:
-#             for x_vect in self.model[list(self.model.keys())[0]]:
-#                 for x in x_vect:
-#                     b-=x*w
-#         return b
+class Function_for_svm(gradient.Function_class):
+    def __init__(self,model):
+        self.model=model
+
+    def function(self, param):
+        first_part=sum(param)
+        #second_part=0.5*math.pow(sum([a*x*y for y in self.model.keys() for x in self.model[y] for a in param]),2)
+        second_part=0
+        tmp_sum=0
+        for l in param:
+            for y in self.model.keys():
+                for x_vect in self.model[y]:
+                    for x in x_vect:
+                        tmp_sum+=y*l
+                        second_part+=l*y*x
+        if(tmp_sum!=0): return -(first_part-second_part*second_part)
+        return first_part-second_part*second_part/2
+
+    def derivative(self, param):
+        return sum([a*y for y in self.model.keys() for a in param]) #actually, we don't use this, so doesn't matter
+
+    def derivative_by_part_x(self, x, index_of_x, vector_of_x):
+        sum=0
+        tmp_sum=0
+        for l in vector_of_x:
+            for y in self.model.keys():
+                for i in range(len(self.model[y])):
+                    if i != index_of_x:
+                        for x in self.model[y][i]:
+                                tmp_sum+=l*y
+                                sum += l * y * x
+        if(tmp_sum!=0):return -sum;
+        return sum
+
+    def get_w(self,lambdas):
+        w=[]
+        for l in lambdas:
+            for y in self.model.keys():
+                for x_vect in self.model[y]:
+                    for x in x_vect:
+                            w.append(x*y*l)
+        return w
+    def get_b(self, lambdas):
+        #b=1/list(model.keys())[0]-sum([x*w for x in self.model[list(self.model.keys())[0]] for w in koefs])
+        koefs=self.get_w(lambdas)
+        b=1/list(self.model.keys())[0]
+        for w in koefs:
+            for x_vect in self.model[list(self.model.keys())[0]]:
+                for x in x_vect:
+                    b-=x*w
+        return b
 
 
 class Support_Vector_Machine:
-    #def __init__(self, visualization=True):
-        # self.colors = {1: 'r', -1: 'b'}
-        # if self.visualization:
-        #     self.fig = plt.figure()
-        #     self.ax = self.fig.add_subplot(1, 1, 1)
 
-    # train
+    def line(self,x, w, b, v):
+        return (-w[0] * x - b + v) / w[1]
+
     def fit(self, data):
         self.data = data
         # { ||w||: [w,b] }
@@ -76,11 +75,7 @@ class Support_Vector_Machine:
 
         # finding values to work with for our ranges.
         all_data = []
-        for yi in self.data:
-            for featureset in self.data[yi]:
-                for feature in featureset:
-                    all_data.append(feature)
-
+        all_data.extend([x[i][c] for x in self.data.values() for i in range(len(x)) for c in range(len(x[i]))])
         self.max_feature_value = max(all_data)
         self.min_feature_value = min(all_data)
         # no need to keep this memory.
@@ -95,7 +90,6 @@ class Support_Vector_Machine:
         b_range_multiple = 5
         b_multiple = 5
         latest_optimum = self.max_feature_value * 10
-
 
         for step in step_sizes:
             w = np.array([latest_optimum,latest_optimum])
@@ -145,40 +139,50 @@ class Support_Vector_Machine:
 if(__name__=="__main__"):
     model = regression.read_model("../svm/points.csv")
     data_dict={i:np.array(model[i]) for i in model.keys()}
-    svm=Support_Vector_Machine()
-    opt_dict=svm.fit(data_dict)
-    print(opt_dict)
+    # svm=Support_Vector_Machine()
+    # w,b=svm.fit(data_dict)
+    # print("w=",w," b=",b)
 
+    func=Function_for_svm(model)
+    koefs=gradient.gradient_descent(6,0,10,0.000000000001,0.001,func,2000,False)
+    b=func.get_b(koefs)
+    print(b)
+    w=func.get_w(koefs)
+    print(w)
 
-    # func=Function_for_svm(model)
-    # print(model)
-    # koefs=gradient.gradient_descent(1,-10,10,0.00000000001,0.01,func,2000,False)
-    # b=func.get_b(koefs)
-    # print(b)
-    # w=func.get_w(koefs)
-    # print(w)
+    # line_points ={}
+    # line_points[-20]=svm.line(-20,w,b,0)
+    # line_points[20] = svm.line(20, w, b, 0)
+    #
+    # first_svm={}
+    # first_svm[-20] = svm.line(-20, w, b, -1)
+    # first_svm[20] = svm.line(20, w, b, -1)
+    #
+    #
+    # second_svm={}
+    # second_svm[-20] = svm.line(-20, w, b, 1)
+    # second_svm[20] = svm.line(20, w, b, 1)
 
-    # line_points = {}
-    # for i in range(70):
-    #     y = koefs[0] * i+b
-    #     line_points[i] = y
-    #
-    # p = figure(title="SVM", x_axis_label='x', y_axis_label='y')
-    #
-    # points1x=[x[0] for x in model[-1]]
-    # points1y=[x[1] for x in model[-1]]
-    # points2x=[x[0] for x in model[1]]
-    # points2y=[x[1] for x in model[1]]
-    # p.circle(points1x,points1y, size=20, legend="Points 1.", color="navy",
-    #          alpha=0.5)
-    # p.circle(points2x, points2y, size=20, legend="Points 2.", color="orange",
-    #          alpha=0.5)
-    #
+    p = figure(title="SVM", x_axis_label='x', y_axis_label='y')
+
+    points1x=[x[0] for x in model[-1]]
+    points1y=[x[1] for x in model[-1]]
+    points2x=[x[0] for x in model[1]]
+    points2y=[x[1] for x in model[1]]
+    p.circle(points1x,points1y, size=20, legend="Points 1.", color="navy",
+             alpha=0.5)
+    p.circle(points2x, points2y, size=20, legend="Points 2.", color="orange",
+             alpha=0.5)
+
     # p.line(list(line_points.keys()), list(line_points.values()), legend="SVM.",
-    #         color="red", line_width=2)
+    #         color="green", line_width=2)
+    #
+    # p.line(list(first_svm.keys()), list(first_svm.values()), legend="support vector=-1",
+    #        color="blue", line_width=1)
+    # p.line(list(second_svm.keys()), list(second_svm.values()), legend="support vector=1",
+    #        color="gray", line_width=1)
+
     # p.line(list(pointsByGradient.keys()), list(pointsByGradient.values()), legend="Linear regression line by gradient.",
     #        color="violet", line_width=2)
-    #output_file("svm.html")
-    # p.line(list(pointsLogistic.keys()),list(pointsLogistic.values()),legend="Logistic.", color="violet",line_width=2)
-    #save(p)
-
+    output_file("svm.html")
+    save(p)
